731d4e7ba7819f9ab273145b419968ea75bc8362
naknak <cnh6123@naver.com>
2021-09-08 21:54:14 +0900

    [update] src 하위 폴더 구조 변경 및 webpack 설정 변경

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
     mode: "none",
-    entry: "./src/index.js",
+    entry: [
+        path.resolve(__dirname, "./src/javascripts/index.js"),
+        path.resolve(__dirname, "./src/stylesheets/style.scss"),
+    ],
     output: {
         path: path.resolve(__dirname, "./dist"),
         filename: "build.js",
@@ -17,10 +20,13 @@ const config = {
                 use: "babel-loader",
                 exclude: /node_modules/,
             },
-            { test: /\.css$/, use: "css-loader" },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
             {
                 test: /\.scss$/,
-                use: ["css-loader", "sass-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
             {
                 test: /\.(png|jpe?g|gif|svg|ico)$/,
